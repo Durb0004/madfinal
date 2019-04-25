f83e08e02f099f4e06305da2a6a4a2848562fb02
Durbano <durb0004@algonqiunlive.com>
2019-04-24 20:15:12 -0400

    updated

--- startup/database.js
+++ startup/database.js
@@ -1,16 +1,30 @@
 const debug = require('debug')('week7:db')
 const mongoose = require('mongoose')
+const config = require('config')
+const logger = require('./logger')
 
 module.exports = () => {
-  mongoose
-    .connect(`mongodb://localhost:27017/MADFINAL`, {
-      useNewUrlParser: true
-    })
-    .then(() => {
-      debug(`Connected to MongoDB ...`)
-    })
-    .catch(err => {
-      debug(`Error connecting to MongoDB ...`, err)
-      process.exit(1)
-    })
+  const mongoose = require('mongoose')
+  const db = config.get('db')
+
+  let credentials = ''
+  if (process.env.NODE_ENV === 'production') {
+    credentials = `${db.username}:${db.password}@`
+  }
+
+  const connectionString = `mongodb://${credentials}${db.host}:${db.port}/${db.name}?authSource=admin`
+
+
+  // module.exports = () => {
+  //   mongoose
+  //     .connect(`mongodb://localhost:27017/MADFINAL`, {
+  //       useNewUrlParser: true
+  //     })
+  //     .then(() => {
+  //       debug(`Connected to MongoDB ...`)
+  //     })
+  //     .catch(err => {
+  //       debug(`Error connecting to MongoDB ...`, err)
+  //       process.exit(1)
+  //     })
 }
